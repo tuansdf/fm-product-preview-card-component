754c344196ca71bbdb36388380fe8636d19ef757
tuansdf <103062682+tuansdf@users.noreply.github.com>
2023-06-10 12:52:29 +0700

    add attribution

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,45 +2,50 @@ import clsx from "clsx";
 import { headingFont } from "~/app/fonts";
 import { CartIcon } from "~/icons/cart-icon";
 import classes from "./page.module.scss";
+import { Attribution } from "~/components/attribution";
 
 export default function Home() {
   return (
-    <main className={classes["main"]}>
-      <picture className={classes["picture"]}>
-        <source
-          srcSet="/images/image-product-desktop.jpg"
-          media="(min-width: 800px)"
-        />
-        <img
-          loading="lazy"
-          decoding="async"
-          src="/images/image-product-mobile.jpg"
-          alt=""
-          className={classes["image"]}
-        />
-      </picture>
+    <>
+      <main className={classes["main"]}>
+        <picture className={classes["picture"]}>
+          <source
+            srcSet="/images/image-product-desktop.jpg"
+            media="(min-width: 800px)"
+          />
+          <img
+            loading="lazy"
+            decoding="async"
+            src="/images/image-product-mobile.jpg"
+            alt=""
+            className={classes["image"]}
+          />
+        </picture>
 
-      <div className={classes["body"]}>
-        <div>
-          <h1 className={classes["category"]}>Perfume</h1>
-          <h2 className={clsx(headingFont.className, classes["title"])}>
-            Gabrielle Essence Eau De Parfum
-          </h2>
+        <div className={classes["body"]}>
+          <div>
+            <h1 className={classes["category"]}>Perfume</h1>
+            <h2 className={clsx(headingFont.className, classes["title"])}>
+              Gabrielle Essence Eau De Parfum
+            </h2>
+          </div>
+          <p className={classes["content"]}>
+            A floral, solar and voluptuous interpretation composed by Olivier
+            Polge, Perfumer Creator for the House of CHANEL.
+          </p>
+          <div className={classes["price"]}>
+            <span className={clsx(headingFont.className, classes["new"])}>
+              $149.99
+            </span>
+            <span className={classes["old"]}>$169.99</span>
+          </div>
+          <button className={classes["button"]}>
+            <CartIcon /> <span>Add to cart</span>
+          </button>
         </div>
-        <p className={classes["content"]}>
-          A floral, solar and voluptuous interpretation composed by Olivier
-          Polge, Perfumer Creator for the House of CHANEL.
-        </p>
-        <div className={classes["price"]}>
-          <span className={clsx(headingFont.className, classes["new"])}>
-            $149.99
-          </span>
-          <span className={classes["old"]}>$169.99</span>
-        </div>
-        <button className={classes["button"]}>
-          <CartIcon /> <span>Add to cart</span>
-        </button>
-      </div>
-    </main>
+      </main>
+
+      <Attribution />
+    </>
   );
 }
